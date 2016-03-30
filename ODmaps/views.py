8656fa24489aaa676e18995db150e28e9332a90a
Cinai <catta.ugs@gmail.com>
2016-03-30 15:34:13 -0300

    no se que es esto

--- ODmaps/views.py
+++ ODmaps/views.py
@@ -16,9 +16,16 @@ def test2(request):
 
 def visualizador(request):
 	sequence_name = 'sequence'
+	usuario = "?"
+	corte_temporal = "?"
 	if request.method == 'GET':
 		sequence_name = request.GET.get('id','sequence')
 		folder_name = request.GET.get('folder','')
+	# asumo que viene en el formato idusuario_indexusuario_mes
+	if(sequence_name!='sequence'):
+		sequence_data = sequence_name.split('_')
+		usuario = sequence_data[1]
+		corte_temporal = sequence_data[2]
 	subida = []
 	bajada = []
 	tiempo = []
@@ -53,7 +60,62 @@ def visualizador(request):
 		"subida": subida,
 		"bajada": bajada,
 		"sequence": sequence,
-		"tipo_dia": tipo_dia
+		"tipo_dia": tipo_dia,
+		"usuario": usuario,
+		"corte_temporal": corte_temporal
+	}
+	js_data = json.dumps(to_json)
+	return render(request,'visualizador.html',{"jsdatos":js_data})
+
+def visualizador_normalizado(request):
+	sequence_name = 'sequence'
+	usuario = "?"
+	corte_temporal = "?"
+	if request.method == 'GET':
+		sequence_name = request.GET.get('id','sequence')
+		folder_name = request.GET.get('folder','')
+	# asumo que viene en el formato idusuario_indexusuario_mes
+	if(sequence_name!='sequence'):
+		sequence_data = sequence_name.split('_')
+		usuario = sequence_data[0]
+		corte_temporal = sequence_data[1]
+	subida = []
+	bajada = []
+	tiempo = []
+	netapa = []
+	nviaje = []
+	tipo_dia = []
+	counter = 0
+	sequence = []
+	dict_paradas = {}
+	file_name =  sequence_name + '.csv'
+	file_path = os.path.join(settings.BASE_DIR,'static','datos',file_name)
+	if(folder_name != ''):
+		file_path = os.path.join(settings.BASE_DIR,'static','datos','sequences_norm2',folder_name,file_name)
+
+	with open(file_path) as csvfile:	
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			#aqui hay que pensaarlo un poco mas, por la bajada
+			tiempo.append(row['tiempo_subida'])
+			tipo_dia.append(row['tipo_dia'])
+			nviaje.append(row['nviaje'])
+			netapa.append(row['netapa'])
+			subida.append(row['par_subida'])
+			bajada.append(row['par_bajada'])
+			dict_paradas = {'subida':subida[counter],'bajada': bajada[counter]}
+			sequence.append(dict_paradas)
+			counter += 1
+	to_json = {
+		"tiempo": tiempo,
+		"nviaje": nviaje,
+		"netapa": netapa,
+		"subida": subida,
+		"bajada": bajada,
+		"sequence": sequence,
+		"tipo_dia": tipo_dia,
+		"usuario": usuario,
+		"corte_temporal": corte_temporal
 	}
 	js_data = json.dumps(to_json)
 	return render(request,'visualizador.html',{"jsdatos":js_data})
